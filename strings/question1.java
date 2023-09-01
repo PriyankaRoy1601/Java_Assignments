e2679d63a883cb83b2449f37b9daa61efa869cdb
Priyanka Roy <142658594+PriyankaRoy1601@users.noreply.github.com>
2023-09-01 09:20:14 +0530

    Add files via upload

---
+++ strings/question1.java
@@ -0,0 +1,22 @@
+package strings;
+
+import java.util.Scanner;
+
+public class question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the string : ");
+		String str=scan.nextLine();
+		int sum=0;
+		for(int i=0;i<str.length();i++){ 
+			if(str.charAt(i)>='0' && str.charAt(i)<='9'){
+				sum+=(str.charAt(i)-'0');
+			}
+		}
+		System.out.println("Sum of all digits in the string " +sum );
+
+	}
+
+}
---
+++ strings/question4.java
@@ -0,0 +1,15 @@
+package strings;
+
+import java.util.Scanner;
+
+public class question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter String : ");
+		String s=scan.nextLine();
+		System.out.println(s.replace("is", "is not"));
+	}
+
+}
